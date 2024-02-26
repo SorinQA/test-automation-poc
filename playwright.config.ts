dcf09931c178f0b5e5d516a4976dd47dba8f8952
Sorin Victor Dimofte <svd@intect.io>
2024-02-26 13:43:29 +0100

    channel chrome

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
     video: 'on',
     trace: 'on-first-retry',
     headless: true,
-    channel: 'chrome'
+    channel: 'chrome',
     baseURL: 'https://testintect.app/',
     // extraHTTPHeaders: {
     //   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
